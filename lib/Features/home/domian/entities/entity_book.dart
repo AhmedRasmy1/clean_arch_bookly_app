e6cd969275296b93cc95dc5b0d234d9ce59ae0f8
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-04 17:23:34 +0300

    create entity book

---
+++ lib/Features/home/domian/entities/entity_book.dart
@@ -0,0 +1,17 @@
+class EntityBook {
+  final String image;
+  final String title;
+  final String author;
+  final num rating;
+  final num price;
+  final int averageCount;
+
+  EntityBook(
+    this.image,
+    this.title,
+    this.author,
+    this.rating,
+    this.price,
+    this.averageCount,
+  );
+}
