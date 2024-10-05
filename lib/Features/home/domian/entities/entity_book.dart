c965be77db92b3bc19522b3f68e1481d97b7fb5e
AhmedRasmy1 <80999027@sci.s-mu.edu.eg>
2024-10-05 21:56:58 +0300

    generate entity books adaptor

--- lib/Features/home/domian/entities/entity_book.dart
+++ lib/Features/home/domian/entities/entity_book.dart
@@ -1,10 +1,21 @@
+import 'package:hive/hive.dart';
+part 'entity_book.g.dart';
+
+@HiveType(typeId: 0)
 class EntityBook {
+  @HiveField(0)
   final String? bookId;
+  @HiveField(1)
   final String? image;
+  @HiveField(2)
   final String? title;
+  @HiveField(3)
   final String? author;
+  @HiveField(4)
   final num? rating;
+  @HiveField(5)
   final num? price;
+  @HiveField(6)
   final num? averageCount;
 
   EntityBook(
